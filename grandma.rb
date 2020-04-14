1631f597186cfea450f95055f61cc78f689aaef0
Cortney Dawkins <cmddawkinscortney@gmail.com>
2020-04-14 19:44:04 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -17,5 +17,7 @@ def speak_to_grandma(phrase)
   if phrase == "I LOVE YOU GRANDMA!"
     return == "I LOVE YOU GRANDMA!"
     elsif phrase == phrase.upcase
-    return 
+    return "NO, NOT SINCE 1938!"
+end
+return answer
 end
